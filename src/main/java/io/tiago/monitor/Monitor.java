bbc1349037995efd97710c23c7b8a55254c308ff
metiago <tiagotg.ribeirogmail.com>
2019-05-15 15:54:09 +0100

    Implement time schedule rules

--- src/main/java/io/tiago/monitor/Monitor.java
+++ src/main/java/io/tiago/monitor/Monitor.java
@@ -7,10 +7,13 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketAddress;
 import java.time.LocalTime;
+import java.util.concurrent.TimeUnit;
 
 public class Monitor implements Runnable {
 
     private static final transient Logger LOGGER = LoggerFactory.getLogger(Monitor.class);
+
+    private static final int MAX_INTERVAL_TIME_IN_SEC = 2;
 
     private Node node;
 
@@ -21,24 +24,53 @@ public class Monitor implements Runnable {
     @Override
     public void run() {
 
-        while (!isScheduledRangeTime(node)) {
+        waitExecution();
 
-            LOGGER.info(String.format("Checking address %s:%s...", this.node.getHost(), this.node.getPort()));
+        long expireAt = node.getExpire();
+
+        while (isInTimeRange(node) && expireAt > 0) {
+
+            LOGGER.info(String.format("Checking address %s:%s", this.node.getHost(), this.node.getPort()));
 
             try (Socket socket = new Socket()) {
-                Thread.sleep(2000);
+
+                TimeUnit.SECONDS.sleep(node.getPollFrequency());
+
                 SocketAddress socketAddress = new InetSocketAddress(this.node.getHost(), this.node.getPort());
                 socket.connect(socketAddress);
                 this.node.setUp(true);
-            } catch (Exception e) {
+            }
+            catch (Exception e) {
                 node.setUp(false);
             }
 
-            LOGGER.info(String.format("Address %s:%s is %s", this.node.getHost(), this.node.getPort(), this.node.isUp()));
+            expireAt--;
+        }
+
+        LOGGER.info(String.format("Address %s:%s is %s", this.node.getHost(), this.node.getPort(), this.node.isUp()));
+    }
+
+    private void waitExecution() {
+
+        while (isTimeScheduled(this.node)) {
+
+            LOGGER.info(String.format("Waiting to check address %s:%s", this.node.getHost(), this.node.getPort()));
+
+            try {
+                TimeUnit.SECONDS.sleep(MAX_INTERVAL_TIME_IN_SEC);
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
-    private boolean isScheduledRangeTime(Node node) {
+    private boolean isTimeScheduled(Node node) {
+        LocalTime now = LocalTime.now();
+        return now.isBefore(node.getStart());
+    }
+
+    private boolean isInTimeRange(Node node) {
         LocalTime now = LocalTime.now();
         return now.isAfter(node.getStart()) && now.isBefore(node.getEnd());
     }
