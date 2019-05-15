3b0b52ca070c2ab92764e785741e40822dca8940
metiago <tiagotg.ribeirogmail.com>
2019-05-15 15:52:29 +0100

    Create a queue for limit nodes

---
+++ src/main/java/io/tiago/monitor/monitor/NodeQueue.java
@@ -0,0 +1,34 @@
+package io.tiago.monitor.monitor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Singleton pattern to control how many node could be added to be monitored
+ */
+public class NodeQueue {
+
+    private static NodeQueue nodeQueue;
+
+    private List<Integer> nodes = new ArrayList<>();
+
+    private NodeQueue() {
+    }
+
+    public static NodeQueue instance() {
+
+        if (nodeQueue == null) {
+            nodeQueue = new NodeQueue();
+        }
+
+        return nodeQueue;
+    }
+
+    public void add(Node node) {
+        nodes.add(node.getPort());
+    }
+
+    public int size() {
+        return this.nodes == null ? 0 : this.nodes.size();
+    }
+}
