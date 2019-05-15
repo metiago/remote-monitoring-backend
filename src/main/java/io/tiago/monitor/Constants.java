de3beb2f5e23ee22689f8e6efe719630db9e87a3
metiago <tiagotg.ribeirogmail.com>
2019-05-15 21:36:45 +0100

    Created constants

---
+++ src/main/java/io/tiago/monitor/Constants.java
@@ -0,0 +1,20 @@
+package io.tiago.monitor;
+
+public class Constants {
+
+    public static final int APP_PORT = 8001;
+
+    public static final String CONTENT_TYPE = "content-type";
+
+    public static final String APPLICATION_TYPE = "application/json";
+
+    public static final String MSG_OK = "Data has been saved successfully";
+
+    public static final String MSG_BAD_REQUEST = "Invalid request body";
+
+    public static final String BODY_NOT_EMPTY = "Body must be not empty";
+
+    public static final String TOO_MANY_NODES = "Cannot add more nodes to the queue";
+
+    public static final int MAX_ALLOWED_NODES = 3;
+}
