0c4e69648509c8364843a0e63f196d1238a62d40
metiago <tiagotg.ribeirogmail.com>
2019-05-20 11:39:25 +0100

    Added generic serializer

--- src/main/java/io/tiago/monitor/helper/JsonHelper.java
+++ src/main/java/io/tiago/monitor/helper/JsonHelper.java
@@ -1,12 +1,19 @@
 package io.tiago.monitor.helper;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import io.vertx.core.json.EncodeException;
 import io.vertx.core.json.Json;
 
+import java.io.IOException;
+
+/**
+ * JsonHelper is responsible to centralize serialization methods as we as to support jdk8 api eg. Localtime
+ */
 public class JsonHelper extends Json {
 
-    // Overridden method to serialize Java 8 Localtime
     public static String encodePrettily(Object obj) {
         try {
             prettyMapper.registerModule(new JavaTimeModule());
@@ -14,5 +21,11 @@ public class JsonHelper extends Json {
         } catch (Exception var2) {
             throw new EncodeException("Failed to encode as JSON: " + var2.getMessage());
         }
+    }
+
+    public static Object readValue(String obj, Class clazz) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new ParameterNamesModule()).registerModule(new Jdk8Module()).registerModule(new JavaTimeModule());
+        return mapper.readValue(obj, clazz);
     }
 }
